029c275966072fdbd464d43396f82598c980c571
Muazam Kamal <muazamkamal.dev@gmail.com>
2020-07-08 14:48:05 +0800

    :sparkles: Added mongodb connection

--- controllers/gears.ts
+++ controllers/gears.ts
@@ -1,8 +1,14 @@
+import { MongoClient } from "https://deno.land/x/mongo@v0.8.0/mod.ts";
 import { Gear } from "../types.ts";
 
-const DB = new Array<Gear>();
+const client = new MongoClient();
+client.connectWithUri(Deno.env.get("MONGO_URI") || "mongodb://localhost:27017");
 
-DB.push({
+const db = client.database("api");
+const gearDb = db.collection("gear");
+
+// Temporary sample load
+const cpu = {
   hardware: "CPU",
   components: [{
     name: "AMD Ryzen™ 5 3600",
@@ -10,10 +16,10 @@ DB.push({
     hyperlink: "https://www.amd.com/en/products/cpu/amd-ryzen-5-3600",
   }],
   banner: "https://muazamkamal.com/img/cpu.915fe0e7.jpg",
-});
+};
 
-const getGears = (ctx: any) => {
-  ctx.response.body = [...DB];
+const getGears = async (ctx: any) => {
+  ctx.response.body = await gearDb.find({ hardware: { $ne: null } });
 };
 
 export { getGears };
