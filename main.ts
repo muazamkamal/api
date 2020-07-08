4e84b4b7453a99a556e57b4c119bf18f5438da0c
Muazam Kamal <muazamkamal.dev@gmail.com>
2020-07-08 16:40:05 +0800

    :bug: Fixed port data type mismatch

--- main.ts
+++ main.ts
@@ -2,7 +2,7 @@ import { Application } from "https://deno.land/x/oak/mod.ts";
 import router from "./routes.ts";
 import RequestError from "./controllers/RequestError.ts";
 
-const port = Deno.env.get("PORT") || 3000;
+const port = parseInt(Deno.env.get("PORT") || "3000");
 const app = new Application();
 
 app.use(async (ctx, next) => {
